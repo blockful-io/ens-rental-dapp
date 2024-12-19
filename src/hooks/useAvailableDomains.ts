af8c550c287d39fce5c3942d2b4d660631d5261e
eduramme <eduramme@gmail.com>
2024-12-19 16:52:39 -0300

    use apollo for useAvailableDomains

--- src/hooks/useAvailableDomains.ts
+++ src/hooks/useAvailableDomains.ts
@@ -1,121 +1,106 @@
-import { useState, useEffect } from "react";
+import {
+  ApolloClient,
+  gql,
+  InMemoryCache,
+  useQuery,
+} from "@apollo/react-hooks";
 import { getEnsRentGraphQL } from "@/src/wagmi";
 import { Address, formatEther } from "viem";
 import { Domain } from "@/src/types";
 import { useChainId } from "wagmi";
+import { useMemo } from "react";
+
+const oneYearInSeconds = 365 * 24 * 60 * 60;
+
+const calculateYearlyPrice = (pricePerSecond: string | number): bigint => {
+  return BigInt(pricePerSecond || 0) * BigInt(oneYearInSeconds);
+};
 
 export default function useAvailableDomains(
   lender: Address | undefined
 ): [Domain[], boolean, Error | null] {
-  const [domains, setDomains] = useState<Domain[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-
   const chainId = useChainId();
+  const ensRentGraphQL = getEnsRentGraphQL(chainId);
 
-  const oneYearInSeconds = 365 * 24 * 60 * 60;
-
-  useEffect(() => {
-    const ensRentGraphQL = getEnsRentGraphQL(chainId);
-
-    const fetchAvailableDomains = async () => {
-      if (!ensRentGraphQL) {
-        setError(new Error("GraphQL endpoint not available"));
-        console.error("GraphQL endpoint not available");
-        setIsLoading(false);
-        return;
-      }
-
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(ensRentGraphQL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            query: `
-              query GetListings {
-                listings(where: ${
-                  !!lender ? `{lender_not: "${lender}"}` : "{}"
-                }) {
-                  items {
-                    rentals {
-                      items {
-                        endTime
-                        borrower
-                      }
-                    }
-                    createdAt
-                    id
-                    isWrapped
-                    lender
-                    maxRentalTime
-                    name
-                    node
-                    price
-                    tokenId
-                  }
-                }
-              }
-            `,
-          }),
-        });
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        if (responseData.errors) {
-          throw new Error(`GraphQL Error: ${responseData.errors[0].message}`);
-        }
-
-        const { data } = responseData;
-
-        if (data?.listings?.items) {
-          const availableDomains = data.listings.items.map(
-            (listing: Domain) => {
-              // Convert price per second to ETH and multiply by seconds in a year
-              const pricePerYear =
-                BigInt(listing.price || 0) * BigInt(oneYearInSeconds);
-              const priceInEth = formatEther(pricePerYear);
-
-              return {
-                id: listing.id,
-                maxRentalTime: listing.maxRentalTime,
-                createdAt: listing.createdAt,
-                isWrapped: listing.isWrapped,
-                lender: listing.lender,
-                node: listing.node,
-                name: `${listing.name}.eth`,
-                price: priceInEth,
-                tokenId: listing.tokenId,
-                rentals: listing.rentals,
-              };
+  const GET_LISTINGS = useMemo(
+    () => `
+    query GetListings {
+      listings(where: ${!!lender ? `{lender_not: "${lender}"}` : "{}"}) {
+        items {
+          rentals {
+            items {
+              endTime
+              borrower
             }
-          );
-
-          const filteredDomains = availableDomains.filter((domain: any) => {
-            const lastRentEndTime = domain?.rentals?.items[0]?.endTime;
-            return !lastRentEndTime || lastRentEndTime < Date.now() / 1000;
-          });
-
-          setError(null);
-          setDomains(filteredDomains);
+          }
+          createdAt
+          id
+          isWrapped
+          lender
+          maxRentalTime
+          name
+          node
+          price
+          tokenId
         }
-      } catch (err) {
-        setError(new Error("An error occurred fetching available domains"));
-        console.error(err);
-      } finally {
-        setIsLoading(false);
       }
-    };
+    }
+  `,
+    [lender]
+  );
 
-    fetchAvailableDomains();
-  }, [lender, chainId]);
+  const client = useMemo(
+    () =>
+      new ApolloClient({
+        uri: ensRentGraphQL,
+        cache: new InMemoryCache(),
+      }),
+    [ensRentGraphQL]
+  );
 
-  return [domains, isLoading, error];
+  const { data, loading, error } = useQuery(
+    gql`
+      ${GET_LISTINGS}
+    `,
+    {
+      client,
+    }
+  );
+
+  if (error) {
+    console.error("Error fetching available domains:", error);
+  }
+
+  const domains = useMemo(() => {
+    let result: Domain[] = [];
+
+    if (data?.listings?.items) {
+      const availableDomains = data.listings.items.map((listing: Domain) => {
+        const pricePerYear = calculateYearlyPrice(listing.price || 0);
+        const priceInEth = formatEther(pricePerYear);
+
+        return {
+          id: listing.id,
+          maxRentalTime: listing.maxRentalTime,
+          createdAt: listing.createdAt,
+          isWrapped: listing.isWrapped,
+          lender: listing.lender,
+          node: listing.node,
+          name: `${listing.name}.eth`,
+          price: priceInEth,
+          tokenId: listing.tokenId,
+          rentals: listing.rentals,
+        };
+      });
+
+      result = availableDomains.filter((domain: any) => {
+        const lastRentEndTime = domain?.rentals?.items[0]?.endTime;
+        return !lastRentEndTime || lastRentEndTime < Date.now() / 1000;
+      });
+    }
+
+    return result;
+  }, [data, oneYearInSeconds]);
+
+  return [domains, loading, error ?? null];
 }
